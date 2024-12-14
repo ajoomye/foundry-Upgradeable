f48a159adcd4cc4658dc748c4c82cd32da40fffe
Abdurraheem Joomye <ajoomye@gmail.com>
2024-12-14 15:38:45 +0800

    first commit

---
+++ script/UpgradeBox.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+import {Script} from "forge-std/Script.sol";
+import {BoxV1} from "src/BoxV1.sol";
+import {BoxV2} from "src/BoxV2.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+
+contract UpgradeBox is Script {
+    
+    function run() external returns (address) {
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("ERC1967Proxy", block.chainid);
+        
+        vm.startBroadcast();
+        BoxV2 newBox = new BoxV2(); //Implementation Logic
+        vm.stopBroadcast();
+        address proxy = upgradeBox(mostRecentDeployed, address(newBox));
+    
+    }
+
+    function upgradeBox(address proxyAddress, address newBox) public returns (address) {
+        
+        vm.startBroadcast();
+        BoxV1 proxy = BoxV1(proxyAddress);
+        proxy.upgradeToAndCall(address(newBox), new bytes(0));
+        vm.stopBroadcast();
+        return address(proxy);
+   
+    }
+
+}
